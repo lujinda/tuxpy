34c4c72f1dc147b4ed4e2951a8ef7217c31fc513
Jinda Lu <q8886888@gmail.com>
2014-12-17 20:30:44 +0800

    后台日志添加页码功能

--- static/js/common.js
+++ static/js/common.js
@@ -1,4 +1,4 @@
-function show(obj,id) {                                                  
+function show(obj,id) {
     var objDiv = $("#"+id+"");
     $(objDiv).html("<li><span>名称/别名:</span> <input name='value' value='"+ obj.innerHTML + "'/></li><li><a OnClick=\"javascript:hide(this, 'sort_msg');\">[关闭]</a><a  href=\"javascript:document.sort_msg_form.action='/tuxpy/listsort.py?set_" + obj.name + "=" + obj.id  + "';document.sort_msg_form.submit();\">[保存]</a></li>");
     $(objDiv).css("display","block");
@@ -8,4 +8,5 @@ function show(obj,id) {
 function hide(obj,id) {
     var objDiv = $("#"+id+"");
     $(objDiv).css("display", "none");
+    event.preventDefault();
 }
